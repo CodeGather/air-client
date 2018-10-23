12ad9a2e269699df2f057116f823e20b3e4c959c
21克的爱情 <raohong07@163.com>
2018-10-23 21:20:42 +0800

    页面提交

--- pages/main/main.vue
+++ pages/main/main.vue
@@ -1,83 +1,121 @@
 <template>
-    <view class="content">
-        <view v-if="hasLogin" class="hello">
-            <view class="title">
-                您好 {{userName}}，您已成功登录。
-            </view>
-            <view class="ul">
-                <view>这是 uni-app 带登录模板的示例App首页。</view>
-                <view>在 “我的” 中点击 “退出” 可以 “注销当前账户”</view>
-            </view>
-        </view>
-        <view v-if="!hasLogin" class="hello">
-            <view class="title">
-                您好 游客。
-            </view>
-            <view class="ul">
-                <view>这是 uni-app 带登录模板的示例App首页。</view>
-                <view>在 “我的” 中点击 “登录” 可以 “登录您的账户”</view>
-            </view>
-        </view>
-    </view>
+	<view>
+		<swiper :style="'min-height:'+height+'px'" :current="active" duration="0" @change="swiperChange">
+			<swiper-item>
+				<view class="swiper-item">进度</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item" style="background-color: #20D785;">发布</view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item" style="background-color: #307DCA;">我的</view>
+			</swiper-item>
+		</swiper>
+		<view class="tab">
+			<view :class="active==0?'item active':'item'" @click="active=0">
+				<view>
+					<view class="uni-icon uni-icon-home"></view>
+				</view>
+				<view>进度</view>
+			</view>
+			<view :class="active==1?'item-add active':'item-add'" @click="active=1">
+				<view>
+					<view class="uni-icon uni-icon-plus-filled"></view>
+				</view>
+				<view class="text">发布</view>
+			</view>
+			<view :class="active==2?'item active':'item'" @click="active=2">
+				<view>
+					<view class="uni-icon uni-icon-person"></view>
+				</view>
+				<view>我的</view>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
-    import {
-        mapState
-    } from 'vuex'
-
-    export default {
-        computed: mapState(['forcedLogin', 'hasLogin', 'userName']),
-        onLoad() {
-            if (!this.hasLogin) {
-                uni.showModal({
-                    title: '未登录',
-                    content: '您未登录，需要登录后才能继续',
-                    /**
-                     * 如果需要强制登录，不显示取消按钮
-                     */
-                    showCancel: !this.forcedLogin,
-                    success: (res) => {
-                        if (res.confirm) {
-							/**
-							 * 如果需要强制登录，使用reLaunch方式
-							 */
-                            if (this.forcedLogin) {
-                                uni.reLaunch({
-                                    url: '../login/login'
-                                });
-                            } else {
-                                uni.navigateTo({
-                                    url: '../login/login'
-                                });
-                            }
-                        }
-                    }
-                });
-            }
-        }
-    }
+	export default {
+		data: {
+			active: 0,
+			height:0
+		},
+		methods:{
+			swiperChange(e) {
+				this.active = e.detail.current;
+			}
+		},
+		onLoad:function(){
+			uni.getSystemInfo({
+				success:res=>{
+					this.height = res.screenHeight;
+				}
+			})
+		}
+	}
 </script>
 
-<style lang="less">
-    .hello {
-        display: flex;
-        flex: 1;
-        flex-direction: column;
-    }
-
-    .title {
-        color: #8f8f94;
-        margin-top: 50px;
-    }
-
-    .ul {
-        font-size: 30px;
-        color: #8f8f94;
-        margin-top: 50px;
-    }
-
-    .ul>view {
-        line-height: 50px;
-    }
+<style lang="scss">
+	@import "../../common/icon.css";
+	.swiper-item {
+		display: flex;
+		width: 100%;
+		height: 100%;
+		text-align: center;
+		background-color: #00BFFF;
+		justify-content: center;
+		align-items: center;
+	}
+	.uni-icon {
+		margin-top: 0;
+		color: #929292;
+	}
+	.tab {
+		position: fixed;
+		z-index: 999;
+		height: 100px;
+		width: 100%;
+		bottom: 0;
+		background-color: #F7F7F7;
+		display: flex;
+		border-top: #CCCCCC 2px solid;
+		& .item-add {
+			width: 100%/3;
+			color: #929292;
+			display: flex;
+			align-items: center;
+			flex-direction: column;
+			justify-content: center;
+			position: relative;
+			text-align: center;
+		}
+	}
+	.item-add .uni-icon {
+		position: absolute;
+		color: #929292;
+		border-radius: 50%;
+		font-size: 100px;
+		left: 25px;
+		top: -50px;
+		box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 1), 0 5px 8px 0 rgba(0, 0, 0, .14), 0 1px 14px 0 rgba(0, 0, 0, .12);
+	}
+	.item-add .text {
+		font-size: 24px;
+		padding-top: 36px;
+	}
+	.tab .item {
+		width: 20%;
+		color: #929292;
+		font-size: 24px;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		justify-content: center;
+	}
+	.active {
+		color: #20D785 !important;
+	}
+	.active .uni-icon {
+		color: #20D785;
+	}
 </style>
