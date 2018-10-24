d41106933999833b45cfb8e9ddfcf60d853a9473
21克的爱情 <raohong07@163.com>
2018-10-24 17:51:57 +0800

    页面修改

--- pages/main/main.vue
+++ pages/main/main.vue
@@ -1,8 +1,8 @@
 <template>
 	<view>
-		<swiper :style="'min-height:'+height+'px'" :current="active" duration="0" @change="swiperChange">
+		<swiper :style="'min-height:'+height+'px;width:'+width+'px'" :current="active" duration="0" @change="swiperChange">
 			<swiper-item>
-				<view class="swiper-item">进度</view>
+				<view class="swiper-item">进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度进度</view>
 			</swiper-item>
 			<swiper-item>
 				<view class="swiper-item" style="background-color: #20D785;">发布</view>
@@ -12,19 +12,19 @@
 			</swiper-item>
 		</swiper>
 		<view class="tab">
-			<view :class="active==0?'item active':'item'" @click="active=0">
+			<view :class="active==0?'item active':'item'" @tap="active=0">
 				<view>
 					<view class="uni-icon uni-icon-home"></view>
 				</view>
 				<view>进度</view>
 			</view>
-			<view :class="active==1?'item-add active':'item-add'" @click="active=1">
+			<view :class="active==1?'item active':'item'" @tap="active=1">
 				<view>
 					<view class="uni-icon uni-icon-plus-filled"></view>
 				</view>
 				<!-- <view class="text">发布</view> -->
 			</view>
-			<view :class="active==2?'item active':'item'" @click="active=2">
+			<view :class="active==2?'item active':'item'" @tap="active=2">
 				<view>
 					<view class="uni-icon uni-icon-person"></view>
 				</view>
@@ -38,17 +38,19 @@
 	export default {
 		data: {
 			active: 0,
-			height:0
+			height: 0,
+			width: 0
 		},
 		methods:{
 			swiperChange(e) {
 				this.active = e.detail.current;
 			}
 		},
-		onLoad:function(){
+		mounted(){
 			uni.getSystemInfo({
 				success:res=>{
 					this.height = res.screenHeight;
+					this.width = res.screenWidth;
 				}
 			})
 		}
@@ -58,18 +60,17 @@
 <style lang="less">
 	@import "../../common/icon.css";
 	@tabWidth:100%/3;
+	@mainColor:#20D785;
+	@auxColor:#929292;
+	@whiColor:#ffffff;
 	.swiper-item {
 		display: flex;
 		width: 100%;
 		height: 100%;
 		text-align: center;
-		background-color: #00BFFF;
+		background-color: @whiColor;
 		justify-content: center;
 		align-items: center;
-	}
-	.uni-icon {
-		margin-top: 0;
-		color: #929292;
 	}
 	/* 底部导航样式 */ 
 	.tab {
@@ -78,48 +79,36 @@
 		height: 100px;
 		width: 100%;
 		bottom: 0;
-		background-color: #F7F7F7;
+		background-color: @whiColor;
 		display: flex;
 		border-top: #CCCCCC 2px solid;
-		/* 导航中间凸起部分 */ 
-		& .item-add {
-			width: @tabWidth;
-			color: #929292;
-			display: flex;
-			align-items: center;
-			flex-direction: column;
-			justify-content: center;
-			position: relative;
-			text-align: center;
-			& .uni-icon {
-				position: absolute;
-				color: #929292;
-				border-radius: 50%;
-				font-size: 100px;
-				left: 25px;
-				top: -50px;
-				box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 1), 0 5px 8px 0 rgba(0, 0, 0, .14), 0 1px 14px 0 rgba(0, 0, 0, .12);
-			}
-			& .text {
-				font-size: 24px;
-				padding-top: 36px;
-			}
-		}
 		/* 底部导航子选项 */ 
 		& .item {
 			width: @tabWidth;
-			color: #929292;
+			color: @auxColor;
 			font-size: 24px;
 			display: flex;
 			align-items: center;
 			flex-direction: column;
 			justify-content: center;
+			/* 导航中间凸起部分 */ 
+			&:nth-child(2) {
+				& .uni-icon {
+					border-radius: 50%;
+					font-size: 100px;
+					margin-top:-40px;
+					background: @whiColor;
+					padding:5px;
+					// box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 1), 0 5px 8px 0 rgba(0, 0, 0, .14), 0 1px 14px 0 rgba(0, 0, 0, .12);
+				}
+				& .text {
+					font-size: 24px;
+					padding-top: 36px;
+				}
+			}
+			&.active {
+				color: @mainColor!important;
+			}
 		}
-	}
-	.active {
-		color: #20D785 !important;
-	}
-	.active .uni-icon {
-		color: #20D785;
 	}
 </style>
